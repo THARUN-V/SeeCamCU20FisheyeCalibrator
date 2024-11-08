8627eda10f9601b8c9103701d82ce55285945128
Tharun V <tharun.v@hachidorirobotics.com>
2024-11-08 11:41:19 +0530

    reomodified code to stream image to webpage for calibration node

--- WebApp.py
+++ WebApp.py
@@ -1,9 +1,45 @@
 from CamContext import *
-from flask import Flask , render_template , request , jsonify , url_for
+from flask import Flask , render_template , request , jsonify , url_for , Response
 import json
 import re
 
 from CalibrationNode import *
+
+
+class SeeCamCalibrationNode:
+    
+    def __init__(self):
+        self.node = None
+        
+    def initialize_calibration_node(self,cam_index):
+        if self.node is None:
+            boards = [ChessboardInfo(6,4,0.04)]
+            calib_flags = 0
+            fisheye_calib_flags = 0
+            checkerboard_flags = cv2.CALIB_CB_FAST_CHECK
+            
+            self.node = OpenCVCalibrationNode(boards,
+                                        calib_flags,
+                                        fisheye_calib_flags,
+                                        checkerboard_flags = checkerboard_flags,
+                                        max_chessboard_speed = -1.0,
+                                        queue_size = 1,
+                                        cam_index = cam_index)
+            
+    def generate_frames(self):
+        if self.node is not None:
+            while True:
+                if self.node.queue_display.qsize() > 0:
+                    frame = self.node.queue_display.get()
+                    ret, buffer = cv2.imencode('.jpg', frame)
+                    if not ret:
+                        continue
+                    frame = buffer.tobytes()
+                    yield (b'--frame\r\n'
+                        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+        
+
 
 class WebApp(CamContext):
     
@@ -19,6 +55,8 @@ class WebApp(CamContext):
         
         # variable to keep track and update table
         self.data = self.update_cam_details()
+        
+        self.calib_node = SeeCamCalibrationNode()
         
     def update_cam_details(self):
         """
@@ -78,6 +116,7 @@ class WebApp(CamContext):
                     
                     see_cam = {cam.serial_number : cam.camera_index for cam in self.get_seecam()}
                     
+                    """
                     boards = [ChessboardInfo(6,4,0.04)]
                     calib_flags = 0
                     fisheye_calib_flags = 0
@@ -100,12 +139,26 @@ class WebApp(CamContext):
                                 row_data["processed"] = True
                                 # Return a success response to the frontend
                                 return jsonify({"message":"Processing completed successfullty"}),200
+                    """
+                    
+                    self.calib_node.initialize_calibration_node(see_cam[row_data["SerialNumber"]])
+                    
+                    row_data["processed"] = True
+                    # Return a success response to the frontend
+                    return jsonify({"message":"Processing completed successfullty"}),200
                     
                 else:
                     return jsonify({"message":"Serial Number not found."}),404
             except Exception as e:
                 # In case of an error, return an error message
                 return jsonify({"message":f"Error during processing : {str(e)}"}) , 500
+            
+    
+        @self.app.route("/video_feed")
+        def video_feed():
+            return Response(self.calib_node.generate_frames(),
+                            mimetype='multipart/x-mixed-replace; boundary=frame')
+            
         
     def run(self):
         self.app.run(debug=True,use_reloader=False)
